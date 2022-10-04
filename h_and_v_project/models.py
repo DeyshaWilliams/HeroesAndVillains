99195870f67133d17d2d92960e11876ac48a5a67
Deysha Williams <deyshawilliams95@gmail.com>
2022-10-03 19:46:52 -0700

    Added urls.py and serializers.py to both h_and_v and super_types apps + Created both supers and super_types classes

--- h_and_v_project/models.py
+++ h_and_v_project/models.py
@@ -1,3 +1,12 @@
+from ftplib import MAXLINE
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
+    
