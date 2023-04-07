d95b80bbcd4581eb6c7260ec2f6f7004640e2b07
Deysha Williams <deyshawilliams95@gmail.com>
2023-04-07 15:49:06 -0700

    Fixed errors + Found issue in sql db downloaded from team + This is how the code should look like anyways, so I'm uploading it, idc

--- super_types/serializers.py
+++ super_types/serializers.py
@@ -5,4 +5,4 @@ from .models import Super_type
 class Super_type_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Super_type
-        fields = ['type']
+        fields = ['id', 'type']
